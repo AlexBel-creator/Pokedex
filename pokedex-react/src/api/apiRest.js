2f0d69cc36c4fdd4274df46f1d18c517f87ddb80
alex <alexandre.bellamy@hetic.eu>
2024-03-07 01:13:19 +0100

    Conect to Pokeapi

---
+++ pokedex-react/src/api/apiRest.js
@@ -0,0 +1,9 @@
+
+// Appel ts les Pokemons
+export const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon";
+
+// Especes des Pokemons
+export const URL_ESPECIES = "https://pokeapi.co/api/v2/pokemon-species";
+
+// Evolution des Pokemons
+export const URL_EVOLUCIONES = "https://pokeapi.co/api/v2/evolution-chain";
--- pokedex-react/src/pages/home/layout/LayoutHome.jsx
+++ pokedex-react/src/pages/home/layout/LayoutHome.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import css from './layout.module.scss'
 import Header from '../header/Header'
-
+import axios from 'axios'
+import { URL_POKEMON } from '../../../api/apiRest'
 
 export default function LayoutHome() {
+
+
+// Axios 
+  useEffect(() => {
+
+
+    const api = async () => {
+
+      const apiPoke = await axios.get(`${URL_POKEMON}`)
+      console.log(apiPoke.data);
+    }
+
+
+    api()
+
+  }, [])
+
+
   return (
-    <div>
+    <div className={css.layout} >
       <Header/>
     </div>
   )
